730097990a109249126d74e723ca0b32dce4f5bb
reza-shahriari <rezshahriari@gmail.com>
2023-04-04 22:52:27 +0430

    adding small mutation

--- New Text Document.cpp
+++ New Text Document.cpp
@@ -6,8 +6,8 @@ class Genome{
     private:
     string RNA;
     string DNA[2];
-    //completed of each NOCLOTID
-    char completed(char a){
+    //clculate completed of each NOCLOTID
+    char Calculate_completed(char a){
         if(a=='A') return 'T';
         if(a=='T') return 'A';
         if(a=='C') return 'G';
@@ -26,9 +26,33 @@ class Genome{
         string DNA_maker[2];
         DNA_maker[0] = input_RNA;
         for(int i = 0 ; i < input_RNA.length() ; i++){
-            DNA_maker[1][i] = completed(DNA_maker[0][i]);
+            DNA_maker[1][i] = Calculate_completed(DNA_maker[0][i]);
         }
         cout<<"created DNA is :\n";
         cout<<DNA_maker[0]<<endl<<DNA_maker[1]<<endl;
     }
+    //creatign small mutation in RNA and DNA
+    string Create_small_mutation(char A , char C, int n){
+        int check = n;
+        for(int i = 0; i < RNA.length();i++){
+            if(RNA[i] ==A){
+                check--;
+                RNA[i] = C;
+                if(check == 0)
+                    break;
+            }
+        }
+        check = n;
+        for(int i = 0; i < RNA.length();i++){
+            if(DNA[0][i] ==A){
+                check--;
+                DNA[0][i] = C;
+                DNA[1][i] = Calculate_completed(C);
+                if(check == 0)
+                    break;
+            }
+        }
+
+    }
+
 };
