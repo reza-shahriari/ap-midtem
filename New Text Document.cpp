345510d664cd6bf4bd4c797ad1d3f115fde4f2a1
reza-shahriari <rezshahriari@gmail.com>
2023-04-04 22:43:25 +0430

    updating Genome
    creating some of the variables and functions

--- New Text Document.cpp
+++ New Text Document.cpp
@@ -1,5 +1,34 @@
 #include<iostream>
+#include<string>
+using namespace std;
 
 class Genome{
+    private:
+    string RNA;
+    string DNA[2];
+    //completed of each NOCLOTID
+    char completed(char a){
+        if(a=='A') return 'T';
+        if(a=='T') return 'A';
+        if(a=='C') return 'G';
+        if(a == 'G') return 'C';
 
+    }
+    public:
+    //return RNA
+    string Get_RNA() const { return RNA; }
+    //return DNA both in a single string 
+    string Get_DNA() const {
+        return DNA[0] + '/' + DNA[1];
+    }
+    //creating DNA form RNA
+    string Create_DNA(string input_RNA){
+        string DNA_maker[2];
+        DNA_maker[0] = input_RNA;
+        for(int i = 0 ; i < input_RNA.length() ; i++){
+            DNA_maker[1][i] = completed(DNA_maker[0][i]);
+        }
+        cout<<"created DNA is :\n";
+        cout<<DNA_maker[0]<<endl<<DNA_maker[1]<<endl;
+    }
 };
