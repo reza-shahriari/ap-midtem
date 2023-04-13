9701b9b31975deb40763f8e69bbe9ec1aadbf429
reza-shahriari <rezshahriari@gmail.com>
2023-04-13 13:33:24 +0430

    Debugging cell and ading animal

--- Cell.cpp
+++ Cell.cpp
@@ -4,9 +4,25 @@
 #include "Genome.h"
 using namespace std;
 
-class Cell : Genome
+class Cell : public Genome
 {
 public:
+    vector<Genome> cromozom_vector;
+    int cunst;
+    Cell(string input_RNA, string DNA1, string DNA2) : Genome(input_RNA, DNA1, DNA2)
+    {
+        RNA = "";
+        DNA[0] = "";
+        DNA[1] = "";
+        Genome gen("", DNA1, DNA2);
+        cromozom_vector.push_back(gen);
+    }
+    // adding genome to cromozom vector
+    void Add_genome_to_cromozom_vector(Genome gen)
+    {
+        gen.RNA = "";
+        cromozom_vector.push_back(gen);
+    }
     // create string from j to i + j
     // for finging palindrome
     string Str_maker_from_jTojplusi(string str, int j, int i)
@@ -16,10 +32,9 @@ public:
             res += str[z];
         return res;
     }
-    vector<Genome> cromozom_vector;
     // check if this cell should be deleted
     // if A,T 3 times more than G,C or more than 5 misstakes in DNA
-    bool check_for_kill(string a, string b)
+    bool Check_for_kill(string a, string b)
     {
         int n = 0;
         int A_T, G_C;
@@ -41,20 +56,27 @@ public:
         return false;
     }
     // delete coromozom if it should be removed
-    void killing_cell()
+    void Killing_cell()
     {
+        cout << "trying to kill!" << endl;
         for (int i = 0; i < cromozom_vector.size(); i++)
         {
             string DNA0 = cromozom_vector[i].DNA[0];
             string DNA1 = cromozom_vector[i].DNA[1];
-            if (check_for_kill(DNA0, DNA1))
-                delete this;
+            cout << "DNA0:" << DNA0 << "\nDNA1:" << DNA1 << endl;
+            if (Check_for_kill(DNA0, DNA1))
+            {
+                cromozom_vector.erase(cromozom_vector.begin() + i);
+                i--;
+                cout << "DNA 0 is:" << DNA0 << endl;
+            }
         }
     }
     // change first s2 in coromozom m with s1 in coromozom n
     //  and doing it for second DNA
     void Create_big_mutation(string s1, int n, string s2, int m)
     {
+        cout<<"Creating Big Mutation!\n";
         int iter1 = cromozom_vector[n].DNA[0].find(s1);
         string temp1 = cromozom_vector[n].DNA[1];
         int iter2 = cromozom_vector[m].DNA[0].find(s2);
@@ -98,35 +120,59 @@ public:
     {
         string str1, str2;
         int n = cromozom_vector[m].DNA[0].size();
-        string crom = cromozom_vector[m].DNA[0];
-
-        for (int i = 1; i < n; i++)
+        for (int kapa = 0; kapa < 2; kapa++)
         {
-            bool check;
-            for (int j = 0; j < n; j++)
+            string crom = cromozom_vector[m].DNA[kapa];
+            for (int i = 1; i < n; i++)
             {
-                check = true;
-                if (j + i > n)
-                    break;
-                str1 = Str_maker_from_jTojplusi(crom, j, i);
-                if (j + i + i > n)
-                    break;
-                str2 = Str_maker_from_jTojplusi(crom, j + i, i);
-                for (int z = j; z < j + i; z++)
+                bool check;
+                for (int j = 0; j < n; j++)
                 {
-                    if (str1[z] != Calculate_completed(str2[z]))
-                        check = false;
+                    check = true;
+                    if (j + i > n)
+                        break;
+                    str1 = Str_maker_from_jTojplusi(crom, j, i);
+                    if (j + i + i > n)
+                        break;
+                    str2 = Str_maker_from_jTojplusi(crom, j + i, i);
+                    for (int z = j; z < j + i; z++)
+                    {
+                        if (str1[z] != Calculate_completed(str2[z]))
+                            check = false;
+                    }
+                }
+                if (check)
+                {
+                    cout << str1 << ":" << str2 << endl;
                 }
             }
-            if (check)
+        }
+    }
+    void List_cromozoms()
+    {
+        cout << "Listing all cromozoms\n";
+        if (cromozom_vector.size() == 0)
+        {
+            cout << "No cromozoms found\n";
+        }
+        else
+        {
+            for (int i = 0; i < cromozom_vector.size(); i++)
             {
-                cout << str1 << ":" << str2 << endl;
+                cout << cromozom_vector[i].DNA[0] << endl;
             }
         }
     }
 };
 
-int main()
-{
-    Genome gen("AAA", "TTT", "GGG");
-}
+// int main()
+// {
+//     cout << "Welcome to the test!\n"
+//          << endl;
+//     Genome gen("", "TTTT", "GGGG");
+//     Genome gen2("", "AGCAGC", "CCCCCC");
+//     Cell cell("", "TTTT", "GGGG");
+//     cell.Add_genome_to_cromozom_vector(gen2);
+//     cell.Create_big_mutation("TTTT", 0, "AGCAG", 1);
+//     cell.List_cromozoms();
+// }
---
+++ animal.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include "Cell.h"
+#include "Genome.h"
+
+using namespace std;
+
+int main(){
+    
+}
